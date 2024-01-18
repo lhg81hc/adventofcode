8f2e4a371b1105374ef330248002d79b1411c59c
Thad Le <llengocthangg@gmail.com>
2024-01-18 21:56:45 +0700

    Fix answer for part 2 of day 4

--- 2023/day_4/part_2/scratchcard_counter.rb
+++ 2023/day_4/part_2/scratchcard_counter.rb
@@ -1,17 +1,12 @@
 require_relative '../scratchcard_reader'
 require_relative 'tree_node'
 
-# {1=>1, 2=>2, 3=>4, 4=>8, 5=>16, 6=>32, 7=>64, 8=>127, 9=>254, 10=>508, 11=>1016, 12=>2000, 13=>3807, 14=>7102, 15=>14196, 16=>28122, 17=>56244, 18=>105386, 19=>210772, 20=>335178, 21=>669340, 22=>985498, 23=>985498}
-
 module Day4
   module Part2
     class ScratchcardCounter
       attr_reader :sum
 
       def initialize
-        @sum = 0
-        @map = {}
-        @s = {}
       end
 
       def self.count
@@ -19,41 +14,11 @@ module Day4
       end
 
       def count
-        line_index = 0
+        build_next_scratchcards_map
+        build_copies_position_map
+        build_total_num_of_scratchcard_by_order_map
 
-        loop do
-          update_map(line_index)
-          line_index += 1
-          break if total_matched_numbers == 0 || line_index >= last_line_index
-        end
-
-        puts pp @map
-
-        # loop do
-        #   build_tree_node(nil, line_index)
-        #   line_index += 1
-        #
-        #   break if Day4::ScratchcardReader.new(lines[line_index]).total_matched_numbers == 0 || line_index >= last_line_index
-        # end
-      end
-
-      def update_map(line_index)
-        init_scratchcard_reader(lines[line_index])
-        next_scratchcard_order_numbers = (1 .. total_matched_numbers).map { |n| card_order_number + n }.select { |n| n - 1 <= last_line_index }
-        @map[card_order_number] = next_scratchcard_order_numbers
-      end
-
-      def build_tree_node(parent_value, line_index)
-        init_scratchcard_reader(lines[line_index])
-        update_stat(card_order_number)
-
-        if total_matched_numbers == 0
-          nil
-        else
-          next_scratchcard_order_numbers = (1 .. total_matched_numbers).map { |n| card_order_number + n }.select { |n| n - 1 <= last_line_index }
-          tree_children = next_scratchcard_order_numbers.map { |n| build_tree_node(card_order_number, n - 1) }
-          TreeNode.new(parent_value, tree_children)
-        end
+        puts "Total scratchcards you end up with: #{@num_of_scratchcard_by_order.values.sum}"
       end
 
       def input_file
@@ -76,12 +41,6 @@ module Day4
         @scratchcard_reader
       end
 
-      # def update_stat(order_number)
-      #   @s[order_number] ||= 0
-      #   @s[order_number] += 1
-      #   puts @s
-      # end
-
       def init_scratchcard_reader(line)
         @scratchcard_reader = Day4::ScratchcardReader.new(line)
       end
@@ -94,8 +53,63 @@ module Day4
         scratchcard_reader.card_order_number
       end
 
+      def next_scratchcard_order_numbers
+        (1 .. total_matched_numbers).
+          map { |n| card_order_number + n }.
+          select { |n| n - 1 <= last_line_index }
+      end
+
       def input_path
         File.join(File.dirname(__FILE__), '../input.txt')
+      end
+
+      private
+
+      def build_next_scratchcards_map
+        @next_scratchcards_map =
+          lines.inject({}) do |h, line|
+            init_scratchcard_reader(line)
+            h[card_order_number] = next_scratchcard_order_numbers
+            h
+          end
+      end
+
+      def build_copies_position_map
+        @copies_position_map =
+          begin
+            r = {}
+
+            @next_scratchcards_map.each do |origin_scratchcard, _v|
+              r[origin_scratchcard] = []
+
+              @next_scratchcards_map.each do |k, next_won_scratchcards|
+                if next_won_scratchcards.include?(origin_scratchcard)
+                  r[origin_scratchcard] << k
+                end
+              end
+            end
+
+            r
+          end
+      end
+
+      def build_total_num_of_scratchcard_by_order_map
+        @num_of_scratchcard_by_order =
+          @copies_position_map.keys.inject({}) do |h, order|
+            previous_order = order - 1
+
+            if previous_order.positive?
+              h[order] =
+                (1 .. previous_order).inject(1) do |temp, o|
+                  @copies_position_map[order].include?(o) ? temp += h[o] : temp += 0
+                  temp
+                end
+            else
+              h[order] = 1
+            end
+
+            h
+          end
       end
     end
   end
