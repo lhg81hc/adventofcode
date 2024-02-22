b27de9e9a92a45e7e60f989ff82b271349097244
Thad Le <llengocthangg@gmail.com>
2024-02-22 19:19:09 +0700

    Adjust LineParser to add num_of_folds as
    a parameter of initializer

--- 2023/day_12/line_parser.rb
+++ 2023/day_12/line_parser.rb
@@ -1,17 +1,23 @@
 module Day12
   class LineParser
-    attr_reader :line
+    attr_reader :line, :num_of_folds
 
-    def initialize(line)
+    def initialize(line, num_of_folds: 1)
       @line = line
+      @num_of_folds = num_of_folds
     end
 
     def damage_records
-      @damage_records ||= line.split[0]
+      @damage_records ||=
+        begin
+          copies = Array.new(num_of_folds) { line.split[0] }
+          copies.join('?')
+        end
     end
 
     def size_of_each_contiguous_group_of_damaged_springs
-      @size_of_each_contiguous_group_of_damaged_springs ||= line.split[1].scan(/\w+/).map(&:to_i)
+      @size_of_each_contiguous_group_of_damaged_springs ||=
+        line.split[1].scan(/\w+/).map(&:to_i) * num_of_folds
     end
   end
 end
