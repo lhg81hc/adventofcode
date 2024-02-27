29ad9b0285bcd57f953316543fbf5712d9adbd25
Thad Le <llengocthangg@gmail.com>
2024-02-27 18:44:47 +0700

    Add class Pattern to represent each pattern block parsed from
    the input file

---
+++ 2023/day_13/pattern.rb
@@ -0,0 +1,39 @@
+module Day13
+  class Pattern
+    def initialize(input_lines)
+      @input_lines = input_lines
+    end
+
+    def rows
+      @rows ||= rows_and_cols[0]
+    end
+
+    def cols
+      @cols ||= rows_and_cols[1]
+    end
+
+    private
+
+    def rows_and_cols
+      @rows_and_cols ||=
+        begin
+          rows_tmp = []
+          cols_tmp = []
+
+          @input_lines.each do |line|
+            row = []
+
+            line.each_char.with_index do |char, char_idx|
+              row << char
+              cols_tmp[char_idx] ||= []
+              cols_tmp[char_idx] << char
+            end
+
+            rows_tmp << row
+          end
+
+          [rows_tmp, cols_tmp]
+        end
+    end
+  end
+end
