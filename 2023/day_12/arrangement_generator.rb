ce39dc4c7dceeadef7f8c36eb1d280fb869b5956
Thad Le <llengocthangg@gmail.com>
2024-02-27 15:34:32 +0700

    Improve ArrangementGenerator
    - Remove #idx_of_unknown_condition_springs() and
    - Add another check to verify possible arrangement in case the
    string does not contain any ? character

--- 2023/day_12/arrangement_generator.rb
+++ 2023/day_12/arrangement_generator.rb
@@ -23,53 +23,59 @@ module Day12
         end
     end
 
-    def generate_arrangement(records, idx)
-      # Base case: All '?' are replaced
-      if idx == total_unknown_condition_springs
+    def generate_arrangement(records, l)
+      if l == records.length
         spring_condition_parser = Day12::SpringConditionParser.new(records)
         damaged_group_sizes = spring_condition_parser.damaged_group_sizes
         @arrangements << records.dup if damaged_group_sizes == recorded_damaged_group_sizes
 
-        return true
+        return nil
       end
 
-      records[idx_of_unknown_condition_springs[idx]] = OPERATIONAL
-      if possible_arrangement?(records)
-        generate_arrangement(records, idx + 1)
-      end
+      if records[l] == UNKNOWN
+        records[l] = OPERATIONAL
+        if possible_arrangement?(records)
+          generate_arrangement(records, l + 1)
+        end
 
-      records[idx_of_unknown_condition_springs[idx]] = DAMAGED
-      if possible_arrangement?(records)
-        generate_arrangement(records, idx + 1)
+        records[l] = DAMAGED
+        if possible_arrangement?(records)
+          generate_arrangement(records, l + 1)
+        end
+
+        records[l] = UNKNOWN
+      else
+        generate_arrangement(records, l + 1)
       end
     end
 
     def possible_arrangement?(arrangement)
-      n =
-        arrangement.each_char.inject(0) do |r, c|
-          r += 1 if c == UNKNOWN || c == DAMAGED
-          r
+      damaged_springs_count = 0
+      unknown_springs_count = 0
+
+      arrangement.each_char do |c|
+        unknown_springs_count += 1 if c == UNKNOWN
+        damaged_springs_count += 1 if c == DAMAGED
+      end
+
+      if unknown_springs_count.zero?
+        groups = arrangement.scan(/#+/)
+        return true if groups.length.zero? || groups.length == 1
+
+        groups[0..-2].each.with_index do |group, idx|
+          if recorded_damaged_group_sizes[idx] != group.length
+            return false
+          end
         end
 
-      n >= total_damaged_springs
-    end
-
-    def total_unknown_condition_springs
-      @total_unknown_condition_springs ||= idx_of_unknown_condition_springs.length
+        true
+      else
+        damaged_springs_count + unknown_springs_count >= total_damaged_springs
+      end
     end
 
     def total_damaged_springs
       @total_damaged_springs ||= recorded_damaged_group_sizes.sum
-    end
-
-    def idx_of_unknown_condition_springs
-      @idx_of_unknown_condition_springs ||=
-        begin
-          damaged_condition_records.each_char.with_index.inject([]) do |r, (char, idx)|
-            r << idx if char == '?'
-            r
-          end
-        end
     end
   end
 end
