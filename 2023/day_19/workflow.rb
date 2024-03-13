747603f03333f459f33778bb22c6014b903b071b
Thad Le <llengocthangg@gmail.com>
2024-03-13 18:40:45 +0700

    Add #rules_and_next_workflows_map and #opposite_rule to
    Workflow,
    - #rules_and_next_workflows_map() returns the array of all possible
    combination of rules that can happen given the rule list
    - #opposite_rule() returns the rule with opposite comparison with
    the input one

--- 2023/day_19/workflow.rb
+++ 2023/day_19/workflow.rb
@@ -2,6 +2,13 @@ require_relative 'workflow_rule'
 
 module Day19
   class Workflow
+    OPPOSITE_COMPARISONS = {
+      '>' => '<=',
+      '<' => '>=',
+      '<=' => '>',
+      '>=' => '<',
+    }
+
     attr_reader :val, :x, :m, :a, :s
 
     def initialize(val)
@@ -13,6 +20,32 @@ module Day19
         next_workflow = rule.examine_machine_part(part)
         return next_workflow unless next_workflow.nil?
       end
+    end
+
+    def rules_and_next_workflows_map
+      rules.each_with_index.inject([]) do |r, (rule, rule_idx)|
+        previous_rules =
+          if rule_idx == 0
+            []
+          else
+            rules[0..rule_idx - 1]
+          end
+
+        opposite_prev_rules = previous_rules.map { |pr| opposite_rule(pr) }
+        curr_rules = opposite_prev_rules << rule
+
+        r << { rules: curr_rules, next_workflow: rule.next_workflow }
+        r
+      end
+    end
+
+    def opposite_rule(rule)
+      return nil if rule.non_comparison_rule?
+      raise "Unknown comparison operator #{rule.comparison_operator}" unless OPPOSITE_COMPARISONS.keys.include?(rule.comparison_operator)
+
+      opposite_comparison_operator = OPPOSITE_COMPARISONS[rule.comparison_operator]
+      opposite_rule_str = rule.val.sub(rule.comparison_operator, opposite_comparison_operator)
+      Day19::WorkflowRule.new(opposite_rule_str)
     end
 
     def name
