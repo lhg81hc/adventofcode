8a8c35371053395a60f9b5620c251bdb91762b21
Thad Le <llengocthangg@gmail.com>
2024-03-26 17:05:24 +0700

    Set up for day 17

---
+++ 2023/day_17/part_1/answer.rb
@@ -0,0 +1,48 @@
+module Day17
+  module Part1
+    class Answer
+      INPUT_FILE = '../input.txt'
+
+      def self.run
+        new.run
+      end
+
+      def run
+        light_beam_path = Day16::LightBeamPath.new(contraption)
+        visited_locations_and_directions = light_beam_path.path(0, 0, 'right')
+
+        visited_locations = {}
+        visited_locations_and_directions.keys.each do |k|
+          visited_locations[k.split.first] = true
+        end
+
+        puts_energized_contraption(visited_locations)
+      end
+
+      def contraption
+        @contraption ||= Day16::Contraption.new(input_path)
+      end
+
+      def input_path
+        File.join(File.dirname(__FILE__), INPUT_FILE)
+      end
+
+      def puts_energized_contraption(visited_locations)
+        contraption.grid.each do |grid_row|
+          line =
+            grid_row.map do |component|
+              if visited_locations["#{component.location.y},#{component.location.x}"]
+                '#'
+              else
+                '.'
+              end
+            end
+
+          puts line.join
+        end
+
+        puts "Total number of energized tiles: #{visited_locations.keys.count}"
+      end
+    end
+  end
+end
