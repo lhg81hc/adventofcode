64f153640a0978c71de88200de47cbcfea3e7186
Thad Le <llengocthangg@gmail.com>
2024-04-08 20:59:17 +0700

    Attempt to resolve part 1 of day 17 using backtracking

--- 2023/day_17/min_heat_loss_path_finder.rb
+++ 2023/day_17/min_heat_loss_path_finder.rb
@@ -10,61 +10,70 @@ module Day17
     end
 
     # Idea:
-    # Start in the TOP LEFT block
-    # If all possible paths have been tried return true
-    # Try all possible next blocks
-    #   If the block is valid for next move
-    #     Then mark this [row, column] as part of the solution and recursively check if this block leads to a solution.
-    #       If the current block is a part of a solution then return true.
-    #       If the current block doesn’t lead to a solution then unmark this [row, column] then backtrack and try other blocks.
-    #       If all blocks have been tried and valid solution is not found return false to trigger backtracking.
+    #
+    # Start at the starting point.
+    # For each of the four possible directions (up, down, left, right), try moving in that direction.
+    # If moving in that direction leads to the ending point, return the path taken.
+    # If moving in that direction does not lead to the ending point, backtrack to the previous position and try a different direction.
+    # Repeat steps 2-4 until the ending point is reached or all possible paths have been explored.
     def find_all_paths
-      traversal_history = []
-      find_paths(traversal_history, start_location)
+      current_path = []
+      paths = []
 
+      find_path(current_path, paths, start_location)
+
+      # paths.each do |path|
+      #   puts path.map do |location|
+      #     location.join(',')
+      #   end.join(' ')
+      # end
     end
 
-    def find_minimum_heat_loss_path
-
-    end
-
-    def find_paths(traversal_history, curr_location)
-      return if @count > 1000
+    def find_path(current_path, paths, curr_location)
+      return if @count > 2000
       @count += 1
 
       # Base case: if m == last row and n == last col
-      # if curr_location == goal_location
-      #   return true
-      # end
-
-      curr_block = heat_lost_map.find_block_by_location(curr_location)
-      next_locations = next_valid_locations(traversal_history, curr_block)
-
-      if next_locations.any? { |l| l == goal_location }
-        puts "found"
-        puts "#{traversal_history.map { |t| "[#{t.join(',')}]" }.join(" ")}"
+      if curr_location == goal_location
+        current_path.map { |location| location.join(',') }.join(' ')
+        paths << current_path
         return true
       end
 
+      curr_block = heat_lost_map.find_block_by_location(curr_location)
+      next_locations = next_valid_locations(current_path, curr_block)
+
       next_locations.each do |next_location|
-        traversal_history << curr_location
-        return true if find_paths(traversal_history, next_location)
-        traversal_history.pop
+        current_path << curr_location
+        find_path(current_path, paths, next_location)
+        current_path.pop
+
+        # current_path << curr_location
+        #
+        # if find_path(current_path, next_location)
+        #   puts "found"
+        #   puts "#{current_path.map { |t| "[#{t.join(',')}]" }.join(" ")}"
+        #   return nil
+        #   # puts "pop #{curr_location.join(',')}"
+        # end
+        #
+        # current_path.pop
       end
 
-      false
+
+      # visited[curr_block.name] = nil
     end
 
-    def next_valid_locations(traversal_history, curr_block)
-      # last_three_locations = [traversal_history[-3], traversal_history[-2], traversal_history[-1]].compact
-      last_visited_location = traversal_history[-1]
-      most_recent_locations = [traversal_history[-2], traversal_history[-1], curr_block.location].compact
-      possible_locations = curr_block.adjacent_blocks.map { |b| [b.line_idx, b.char_idx] }
+    def next_valid_locations(current_path, curr_block)
+      # last_three_locations = [current_path[-3], current_path[-2], current_path[-1]].compact
+      last_visited_location = current_path[-1]
+      most_recent_locations = [current_path[-2], current_path[-1], curr_block.location].compact
+      possible_locations = curr_block.adjacent_blocks.map(&:location)
 
-      puts "traversal_history: #{traversal_history.map { |t| "[#{t.join(',')}]" }.join(" ")}"
+      puts "current_path: #{current_path.map { |t| "[#{t.join(',')}]" }.join(" ")}"
       puts "current location: [#{curr_block.line_idx},#{curr_block.char_idx}]"
 
-      possible_locations = possible_locations.select { |l| !traversal_history.include?(l) }
+      possible_locations = possible_locations.select { |l| !current_path.include?(l) }
       possible_locations = possible_locations.select { |l| l[0] != last_visited_location[0] } if three_horizontal_consecutive_blocks?(most_recent_locations)
       possible_locations = possible_locations.select { |l| l[1] != last_visited_location[1] } if three_vertical_consecutive_blocks?(most_recent_locations)
       puts "possible_locations: #{possible_locations.map { |l| "[#{l.join(', ')}]" }.join(' ')}"
@@ -99,49 +108,6 @@ module Day17
 
     def max_possible_heat_lost
       @max_possible_heat_lost ||= 9 * heat_lost_map.height * heat_lost_map.width
-    end
-
-    def dijkstra(source_name)
-      dist = {}
-      q = []
-
-      vertices.each do |vertex|
-        dist[vertex.name] = Float::INFINITY
-        q << vertex
-      end
-
-      dist[source_name] = 0
-
-      until q.empty?
-        min = nil
-        u_idx = nil
-
-        q.each.with_index do |vertex, idx|
-          if min.nil?
-            min = dist[vertex.name]
-            u_idx = idx
-            next
-          end
-
-          if dist[vertex.name] < min
-            min = dist[vertex.name]
-            u_idx = idx
-          end
-        end
-
-        u = q.delete_at(u_idx)
-
-        u.neighbors.each.with_index do |neighbor, neighbor_idx|
-          next unless q.find { |v| v.name == neighbor.name }
-          alt = dist[u.name] + u.weights[neighbor_idx]
-
-          if alt < dist[neighbor.name]
-            dist[neighbor.name] = alt
-          end
-        end
-      end
-
-      dist
     end
   end
 end
