f5abe6bbb5e378c11a56364611a7b92ce7628c0d
Thad Le <llengocthangg@gmail.com>
2024-04-17 11:10:22 +0700

    Introduce Pulse class

---
+++ 2023/day_20/pulse.rb
@@ -0,0 +1,22 @@
+module Day20
+  class Pulse
+    LOW_PULSE = :low_pulse
+    HIGH_PULSE = :high_pulse
+
+    attr_reader :value
+
+    def initialize(value, from_module_name, to_module_name)
+      @value = value
+      @from_module_name = from_module_name
+      @to_module_name = to_module_name
+    end
+
+    def low?
+      value == LOW_PULSE
+    end
+
+    def high?
+      value == HIGH_PULSE
+    end
+  end
+end
