db647d7b0694d76a759ad65a6ec77d76561618a6
Thad Le <llengocthangg@gmail.com>
2024-04-25 19:29:36 +0700

    Change SnapshotParser#parse to return an instance of
    BricksArrangement instead of an array of Brick

--- 2023/day_22/snapshot_parser.rb
+++ 2023/day_22/snapshot_parser.rb
@@ -1,4 +1,5 @@
 require_relative 'brick'
+require_relative 'bricks_arrangement'
 
 module Day22
   class SnapshotParser
@@ -13,13 +14,16 @@ module Day22
     end
 
     def parse
+      bricks_arrangement = Day22::BricksArrangement.new
+
       File.foreach(filepath).with_index.reduce([]) do |r, (line, line_index)|
         brick_name = line_index + 1
         first_coordinates_set, second_coordinates_set = line.split('~').map { |s| s.split(',').map(&:to_i) }
 
-        r << Day22::Brick.new(brick_name, first_coordinates_set, second_coordinates_set)
-        r
+        bricks_arrangement.add_brick(Day22::Brick.new(brick_name, first_coordinates_set, second_coordinates_set))
       end
+
+      bricks_arrangement
     end
   end
 end
