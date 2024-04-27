a27c22bc31fd2c56f9463b1b4c44ac5aeda70269
Thad Le <llengocthangg@gmail.com>
2024-04-27 23:32:43 +0700

    Remove unused Brick#num_of_cubes() method

--- 2023/day_22/brick.rb
+++ 2023/day_22/brick.rb
@@ -1,12 +1,11 @@
 module Day22
   class Brick
-    attr_reader :name, :first_coordinates_set, :second_coordinates_set, :num_of_cubes
+    attr_reader :name, :first_coordinates_set, :second_coordinates_set
 
     def initialize(name, first_coordinates_set, second_coordinates_set)
       @name = name
       @first_coordinates_set = first_coordinates_set
       @second_coordinates_set = second_coordinates_set
-      @num_of_cubes = get_num_of_cubes
     end
 
     def update_start_z_coordinate(z_coordinate)
@@ -31,21 +30,6 @@ module Day22
       end
     end
 
-    def get_num_of_cubes
-      differences =
-        first_coordinates_set.each.with_index.inject([]) do |r, (coordinate, idx)|
-          r << (second_coordinates_set[idx] - coordinate + 1) if second_coordinates_set[idx] != coordinate
-          r
-        end
-
-      num_of_diff = differences.length
-
-      if num_of_diff.zero?
-        1
-      elsif num_of_diff == 1
-        differences.first
-      else
-        raise "Invalid brick coordinates #{name}"
       end
 
     end
