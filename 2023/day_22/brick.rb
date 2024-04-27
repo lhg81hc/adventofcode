24230db1825827894486c12260d1d3c71b319935
Thad Le <llengocthangg@gmail.com>
2024-04-27 17:59:41 +0700

    Add new methods to Brick to get the ordered value of each
    coordinate

--- 2023/day_22/brick.rb
+++ 2023/day_22/brick.rb
@@ -17,6 +17,18 @@ module Day22
       @first_coordinates_set.last = z_coordinate
     end
 
+    { 'x': 0, 'y': 1, 'z': 2 }.each do |coordinate_name, idx|
+      define_method("ordered_#{coordinate_name}_coordinates") do
+        [first_coordinates_set[idx], second_coordinates_set[idx]].minmax
+      end
+
+      define_method("start_#{coordinate_name}_coordinate") do
+        send("ordered_#{coordinate_name}_coordinates").first
+      end
+
+      define_method("end_#{coordinate_name}_coordinate") do
+        send("ordered_#{coordinate_name}_coordinates").last
+      end
     end
 
     def get_num_of_cubes
