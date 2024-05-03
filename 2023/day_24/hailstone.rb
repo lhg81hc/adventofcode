e248a345aa5fad2c388ab13b272f687ed80c0daa
Thad Le <llengocthangg@gmail.com>
2024-05-03 18:55:29 +0700

    Add Hailstone class with represent snow hailstones

---
+++ 2023/day_24/hailstone.rb
@@ -0,0 +1,27 @@
+module Day24
+  class Hailstone
+    attr_reader :name, :position, :velocity
+
+    def initialize(name, position, velocity)
+      @name = name
+      @position = position
+      @velocity = velocity
+    end
+
+    def x
+      position[0]
+    end
+
+    def y
+      position[1]
+    end
+
+    def x_velocity
+      velocity[0]
+    end
+
+    def y_velocity
+      velocity[1]
+    end
+  end
+end
