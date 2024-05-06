4e065ca984d234356d5df5d821a1fd57d2a0fffd
Thad Le <llengocthangg@gmail.com>
2024-05-06 09:38:21 +0700

    Init answer for part 2 of day 24

---
+++ 2023/day_24/part_2/answer.rb
@@ -0,0 +1,19 @@
+require_relative '../input_parser'
+require_relative '../testing_area'
+
+module Day24
+  module Part2
+    class Answer
+      def self.run
+        new.run
+      end
+
+      def run
+      end
+
+      def filepath
+        File.join(File.dirname(__FILE__), '../input.txt')
+      end
+    end
+  end
+end
