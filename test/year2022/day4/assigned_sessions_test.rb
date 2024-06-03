82815a3f3cb7bea67304cb040ebfbd8d3c71133d
Thad Le <llengocthangg@gmail.com>
2024-06-03 09:55:33 +0700

    2022 - Add AssignedSessions class which represents the assigned
    sessions of each Elf

---
+++ test/year2022/day4/assigned_sessions_test.rb
@@ -0,0 +1,40 @@
+require 'test/unit'
+require_relative '../../../year2022/day4/assigned_sessions'
+# require './year2022/day4/assigned_sessions'
+
+module Year2022
+  module Day4
+    class AssignedSessionsTest < Test::Unit::TestCase
+      def setup
+        @str1 = '2-4'
+        @str2 = '5-7'
+        @str3 = '6-6'
+        @assigned_sessions1 = Year2022::Day4::AssignedSessions.new(@str1)
+        @assigned_sessions2 = Year2022::Day4::AssignedSessions.new(@str2)
+        @assigned_sessions3 = Year2022::Day4::AssignedSessions.new(@str3)
+      end
+
+      def test_start_session_id
+        assert_equal(2, @assigned_sessions1.start_session_id)
+        assert_equal(5, @assigned_sessions2.start_session_id)
+        assert_equal(6, @assigned_sessions3.start_session_id)
+      end
+
+      def test_end_session_id
+        assert_equal(4, @assigned_sessions1.end_session_id)
+        assert_equal(7, @assigned_sessions2.end_session_id)
+        assert_equal(6, @assigned_sessions3.end_session_id)
+      end
+
+      def test_contain?
+        assert(@assigned_sessions2.contain?(@assigned_sessions3))
+
+        refute(@assigned_sessions1.contain?(@assigned_sessions2))
+        refute(@assigned_sessions1.contain?(@assigned_sessions3))
+        refute(@assigned_sessions2.contain?(@assigned_sessions1))
+        refute(@assigned_sessions3.contain?(@assigned_sessions1))
+        refute(@assigned_sessions3.contain?(@assigned_sessions2))
+      end
+    end
+  end
+end
---
+++ year2022/day4/assigned_sessions.rb
@@ -0,0 +1,28 @@
+module Year2022
+  module Day4
+    class AssignedSessions
+      attr_reader :range_str
+
+      def initialize(range_str)
+        @range_str = range_str
+      end
+
+      def session_ids
+        @session_ids ||= @range_str.split('-').map(&:to_i).sort
+      end
+
+      def start_session_id
+        session_ids[0]
+      end
+
+      def end_session_id
+        session_ids[1]
+      end
+
+      def contain?(another_assigned_sessions)
+        start_session_id <= another_assigned_sessions.start_session_id &&
+          end_session_id >= another_assigned_sessions.end_session_id
+      end
+    end
+  end
+end
