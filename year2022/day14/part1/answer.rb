9ebaec7eac89c84e40281e10c2101e8076cd93a9
Thad Le <llengocthangg@gmail.com>
2024-10-15 18:32:15 +0700

    2023 - Init answer for part 1 of day 14

---
+++ year2022/day14/part1/answer.rb
@@ -0,0 +1,20 @@
+require_relative '../input_parser'
+
+module Year2022
+  module Day14
+    module Part1
+      class Answer
+        def self.run
+          new.run
+        end
+
+        def run
+        end
+
+        def input_path
+          File.join(File.dirname(__FILE__), '../input.txt')
+        end
+      end
+    end
+  end
+end
